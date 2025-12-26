00a1858eb0fb5352b95951c46ec56b074b232a94
Augusto Jesus Rodriguez Linares <aurodrig@c2r7s1.42madrid.com>
2025-12-26 16:33:34 +0100

    c

---
+++ file.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aurodrig <aurodrig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 03:20:24 by fbetul            #+#    #+#             */
+/*   Updated: 2025/12/26 16:28:54 by aurodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIFE
+#define LIFE
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+typedef struct s_game
+{
+	int width;
+	int height;
+	int iterations;
+	char alive;
+	char dead;
+	int i;
+	int j;
+	int draw;
+	char** board;
+} t_game;
+
+int init_game(t_game* game, char* argv[]);
+void fill_board(t_game* game);
+int play(t_game* game);
+void print_board(t_game* game);
+void free_board(t_game* game);
+
+
+#endif
